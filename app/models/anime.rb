d66896307b1401090c4be51c8deff4cf948585b5
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-13 08:13:46 +0900

    バリデーションの設定

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,2 +1,8 @@
 class Anime < ApplicationRecord
+  validates :title, presence: true
+  validates :synopsis, presence: true
+  validates :genre_id, presence: true
+  validates :good_point_id, presence: true
+  validates :recommendation_id, presence: true
+  validates :text, presence: true
 end
