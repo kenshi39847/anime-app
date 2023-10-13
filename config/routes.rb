5eafdefe3ae6ab48dee355626accf76d520f3663
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-13 17:57:49 +0900

    ルーティングの設定とビューファイルの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :animes, only: [:index, :new, :create]
+  resources :animes, only: [:index, :new, :create, :edit]
   resources :users, only: :show
   root to: "animes#index"
 end
