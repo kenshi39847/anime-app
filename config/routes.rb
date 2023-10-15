07606a8314a6337090be4f9671f935d5db537afe
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-15 14:51:19 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :animes
+  resources :animes do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
   root to: "animes#index"
 end
