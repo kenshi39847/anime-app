410a28417683faf3cb6ddbbf61b0a4a580a3d3bd
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-16 16:40:00 +0900

    commentsテーブルの作成とアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :animes
+  has_many :comments
   
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_15_003825) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_16_072543) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_15_003825) do
     t.index ["user_id"], name: "index_animes_on_user_id"
   end
 
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "anime_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["anime_id"], name: "index_comments_on_anime_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -68,4 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_15_003825) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "animes"
+  add_foreign_key "comments", "users"
 end
