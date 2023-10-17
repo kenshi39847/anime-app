ff38c6b7398a77c0935f2c55f164f78a2f322507
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-17 20:00:41 +0900

    アコーディオンメニューの導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,17 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import jquery from "jquery"
 window.$ = jquery
+
+$(function() {
+  $('.question').click(function() {
+    //クリックされた質問の子要素のspan以外からはopenというクラスを外す
+    $('.question').not(this).children('span').removeClass('open');
+    //クリックされた質問部分に対する回答以外は全て閉じる
+    $('.question').not(this).next().slideUp(400);
+    
+    //クリックされた質問の子要素のspanにopenクラスが付与されいなければ付与し、付与されていれば外す
+    $(this).children('span').toggleClass('open');
+    //クリックされた質問に対する回答を表示する
+    $(this).next().slideToggle(400);
+  });
+});
