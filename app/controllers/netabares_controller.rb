f22cf88767ce4543aea31e81da90b13caf32d78b
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-19 21:18:55 +0900

    コントローラーの作成

--- app/controllers/netabares_controller.rb
+++ app/controllers/netabares_controller.rb
@@ -1,17 +1,2 @@
 class NetabaresController < ApplicationController
-  before_action :authenticate_user!
-
-  def create
-    @anime = Anime.find(params[:anime_id])
-    
-    if user_signed_in?
-      if params[:operation] == 'increment'
-        @anime.increment_netabare_count
-      elsif params[:operation] == 'decrement'
-        @anime.decrement_netabare_count
-      end
-    end
-
-    redirect_to @anime, notice: 'ネタバレカウントが更新されました。'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :animes do
-
-    resources :comments, only: :create
-
-    resources :netabares, only: [:create] # ネタバレリソースに対するcreateアクションのルーティング
-    member do
-    put 'increment_netabare_count', to: 'netabares#increment', as: 'increment_netabare_count'
-    put 'decrement_netabare_count', to: 'netabares#decrement', as: 'decrement_netabare_count'
-    end
-
     collection do
       get 'search'
     end
+    resources :comments, only: :create
   end
   resources :users, only: :show
   root to: "animes#index"
