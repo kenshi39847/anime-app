d9ef7a7ce8b1c467b39a508462764580f4edb997
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-23 15:55:34 +0900

    ネタバレカウント機能でボタンを押すと詳細ページへ遷移しカウントが変わる

--- app/controllers/netabares_controller.rb
+++ app/controllers/netabares_controller.rb
@@ -6,9 +6,11 @@ class NetabaresController < ApplicationController
     netabare = current_user.netabares.find_by(anime_id: @anime.id)
     if netabare.present?
       netabare.destroy
+      redirect_to anime_path(netabare.anime.id)
     else
       @netabare = current_user.netabares.build(anime_id: params[:anime_id])
       @netabare.save
+      redirect_to anime_path(@netabare.anime.id)
     end
   end
 
