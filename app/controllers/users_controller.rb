4b8d3e8e2a9953f4f8566b82aeefd3b7cf5b55c8
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-06 09:57:42 +0900

    usersコントローラーの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,24 @@ class UsersController < ApplicationController
       @netabare[anime.id] = Netabare.where(anime_id: anime.id).count
     end
     @profile = @user.profile
+
+    @currentUserEntry=Entry.where(user_id: current_user.id)  #現在ログインしているユーザー
+    @userEntry=Entry.where(user_id: @user.id)  #もう一人のユーザー
+    if @user.id == current_user.id  #もう一人のユーザー=現在ログインしているユーザー
+    else                            #ではない
+      @currentUserEntry.each do |cu|  #ルームが既に作成されている場合
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom  #ルームが既に作られている状態
+      else        #ではない
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 end
