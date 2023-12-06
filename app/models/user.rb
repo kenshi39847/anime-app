b02f48332d0fb71037e673db2b8ab3cdf842af1f
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-06 09:21:36 +0900

    アソシエーションの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,9 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
 
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+
   validates :name, presence: true
 
   def followed_by?(user)
